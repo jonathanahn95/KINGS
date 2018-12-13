424fe2c886d4f8efe323776349afc4d71758fe43
Jonathan Ahn <jonathanahn95@gmail.com>
2018-12-13 14:56:59 -0500

    Refactor code

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1413,7 +1413,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/es/index.js");
 /* harmony import */ var lodash_merge__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! lodash/merge */ "./node_modules/lodash/merge.js");
 /* harmony import */ var lodash_merge__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(lodash_merge__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _product_show__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./product_show */ "./frontend/components/products/product_show.jsx");
+/* harmony import */ var _product_show_item__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./product_show_item */ "./frontend/components/products/product_show_item.jsx");
 /* harmony import */ var react_stars__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react-stars */ "./node_modules/react-stars/dist/react-stars.js");
 /* harmony import */ var react_stars__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react_stars__WEBPACK_IMPORTED_MODULE_4__);
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
@@ -1461,22 +1461,139 @@ function (_React$Component) {
   }, {
     key: "render",
     value: function render() {
-      var product = this.props.product;
-      var users = this.props.users;
-      var rating,
-          description,
-          user,
-          photos,
-          price = null;
+      var _this$props = this.props,
+          product = _this$props.product,
+          users = _this$props.users;
+      var renderProductInfo = null;
 
       if (product && users && users[product.user_id]) {
-        rating = product.rating;
-        description = product.description;
-        photos = product.photos[0].photo_image_url;
-        price = product.price;
-        user = users[product.user_id].fname;
+        renderProductInfo = [product].map(function (prod, idx) {
+          return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_product_show_item__WEBPACK_IMPORTED_MODULE_3__["default"], {
+            product: product,
+            key: idx,
+            users: users
+          });
+        });
       }
 
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, renderProductInfo);
+    }
+  }]);
+
+  return ProductShow;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["withRouter"])(ProductShow));
+
+/***/ }),
+
+/***/ "./frontend/components/products/product_show_container.js":
+/*!****************************************************************!*\
+  !*** ./frontend/components/products/product_show_container.js ***!
+  \****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+/* harmony import */ var _product_show__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./product_show */ "./frontend/components/products/product_show.jsx");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/es/index.js");
+/* harmony import */ var _actions_product_actions__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../actions/product_actions */ "./frontend/actions/product_actions.js");
+/* harmony import */ var _actions_user_actions__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../actions/user_actions */ "./frontend/actions/user_actions.js");
+
+
+
+
+
+
+
+var msp = function msp(state, ownProps) {
+  return {
+    product: state.entities.products[ownProps.match.params.id],
+    users: state.entities.users
+  };
+};
+
+var mapDispatchToProps = function mapDispatchToProps(dispatch) {
+  return {
+    fetchProduct: function fetchProduct(product) {
+      return dispatch(Object(_actions_product_actions__WEBPACK_IMPORTED_MODULE_4__["fetchProduct"])(product));
+    },
+    fetchAllUsers: function fetchAllUsers() {
+      return dispatch(Object(_actions_user_actions__WEBPACK_IMPORTED_MODULE_5__["fetchAllUsers"])());
+    }
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_1__["connect"])(msp, mapDispatchToProps)(_product_show__WEBPACK_IMPORTED_MODULE_2__["default"]));
+
+/***/ }),
+
+/***/ "./frontend/components/products/product_show_item.jsx":
+/*!************************************************************!*\
+  !*** ./frontend/components/products/product_show_item.jsx ***!
+  \************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/es/index.js");
+/* harmony import */ var lodash_merge__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! lodash/merge */ "./node_modules/lodash/merge.js");
+/* harmony import */ var lodash_merge__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(lodash_merge__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react_stars__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-stars */ "./node_modules/react-stars/dist/react-stars.js");
+/* harmony import */ var react_stars__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_stars__WEBPACK_IMPORTED_MODULE_3__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+
+
+
+
+
+var ProductShowItem =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(ProductShowItem, _React$Component);
+
+  function ProductShowItem() {
+    _classCallCheck(this, ProductShowItem);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(ProductShowItem).apply(this, arguments));
+  }
+
+  _createClass(ProductShowItem, [{
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          product = _this$props.product,
+          users = _this$props.users;
+      var rating = product.rating,
+          description = product.description,
+          price = product.price;
+      var photos = product.photos[0].photo_image_url;
+      var user = users[product.user_id].fname;
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", {
         className: "prod-show-h2"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
@@ -1486,7 +1603,7 @@ function (_React$Component) {
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
         className: "prod-show-user-img",
         src: "https://s3.amazonaws.com/kings-2-dev/user_prof.png"
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, user)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_stars__WEBPACK_IMPORTED_MODULE_4___default.a, {
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, user)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_stars__WEBPACK_IMPORTED_MODULE_3___default.a, {
         color2: "black",
         count: 5,
         edit: false,
@@ -1578,55 +1695,10 @@ function (_React$Component) {
     }
   }]);
 
-  return ProductShow;
+  return ProductShowItem;
 }(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
 
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["withRouter"])(ProductShow));
-
-/***/ }),
-
-/***/ "./frontend/components/products/product_show_container.js":
-/*!****************************************************************!*\
-  !*** ./frontend/components/products/product_show_container.js ***!
-  \****************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-/* harmony import */ var _product_show__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./product_show */ "./frontend/components/products/product_show.jsx");
-/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/es/index.js");
-/* harmony import */ var _actions_product_actions__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../actions/product_actions */ "./frontend/actions/product_actions.js");
-/* harmony import */ var _actions_user_actions__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../actions/user_actions */ "./frontend/actions/user_actions.js");
-
-
-
-
-
-
-
-var msp = function msp(state, ownProps) {
-  return {
-    product: state.entities.products[ownProps.match.params.id],
-    users: state.entities.users
-  };
-};
-
-var mapDispatchToProps = function mapDispatchToProps(dispatch) {
-  return {
-    fetchProduct: function fetchProduct(product) {
-      return dispatch(Object(_actions_product_actions__WEBPACK_IMPORTED_MODULE_4__["fetchProduct"])(product));
-    },
-    fetchAllUsers: function fetchAllUsers() {
-      return dispatch(Object(_actions_user_actions__WEBPACK_IMPORTED_MODULE_5__["fetchAllUsers"])());
-    }
-  };
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_1__["connect"])(msp, mapDispatchToProps)(_product_show__WEBPACK_IMPORTED_MODULE_2__["default"]));
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["withRouter"])(ProductShowItem));
 
 /***/ }),
 
--- frontend/components/products/product_show_item.jsx
+++ frontend/components/products/product_show_item.jsx
@@ -2,115 +2,113 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import merge from "lodash/merge";
 import { Link } from "react-router-dom";
+import ReactStars from "react-stars";
 
 class ProductShowItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    const product = this.props.product;
-    const users = this.props.users;
-    let rating = product.rating;
-    let  description = product.description;
-    let  photos = product.photos[0].photo_image_url;
-    let  price = product.price;
-    let  user = users[product.user_id].fname;
-    
-    return   <nav className="prod-show-h2">
-        <ul className="prod-show-user-info">
-          <li className="prod-show-user-star">
-            <img
-              className="prod-show-user-img"
-              src="https://s3.amazonaws.com/kings-2-dev/user_prof.png"
-            />
-            <p>{user}</p>
-          </li>
-          <li>
-            <ReactStars
-              color2={"black"}
-              count={5}
-              edit={false}
-              value={rating}
-              size={18}
-            />
-          </li>
-        </ul>
-        <figure>
-          <img className="prod-show-pictures" src={photos} />
-        </figure>
-      </nav>
-      <section className="prod-show-middle-section">
-        <section>
-          <figure className="prod-show-main-pic-wrapper">
-            <img className="prod-show-main-pic" src={photos} />
+    const { product, users } = this.props;
+    const { rating, description, price } = product;
+    let photos = product.photos[0].photo_image_url;
+    let user = users[product.user_id].fname;
+
+    return (
+      <div>
+        <nav className="prod-show-h2">
+          <ul className="prod-show-user-info">
+            <li className="prod-show-user-star">
+              <img
+                className="prod-show-user-img"
+                src="https://s3.amazonaws.com/kings-2-dev/user_prof.png"
+              />
+              <p>{user}</p>
+            </li>
+            <li>
+              <ReactStars
+                color2={"black"}
+                count={5}
+                edit={false}
+                value={rating}
+                size={18}
+              />
+            </li>
+          </ul>
+          <figure>
+            <img className="prod-show-pictures" src={photos} />
           </figure>
-          <div className="prod-show-main-description">
-            <h2 className="prod-show-main-description-header">Description</h2>
-            <div className="prod-show-main-description-text">
-              {description}
-            </div>
-          </div>
-        </section>
-        <div className="prod-show-form-wrapper">
-          <form className="prod-show-form">
-            <div className="prod-show-description">{description}</div>
-            <div className="prod-show-price-quest">
-              <p className="prod-show-price">{price}</p>
-              <p className="prod-ask-question">Ask a question</p>
-            </div>
-            <div className="prod-show-select-size">
-              <h3 className="prod-show-size">Size</h3>
-              <select className="custom-select" type="size">
-                <option value="Select an option">Select an option</option>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-              </select>
-            </div>
-            <div className="prod-show-select-quantity">
-              <h3 className="prod-show-quantity">Quanity</h3>
-              <select className="custom-select-2" type="quantity">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-              </select>
-            </div>
-            <input
-              className="prod-show-form-submit"
-              type="submit"
-              value="Add to Cart"
-            />
-          </form>
-          <div className="prod-show-like-wrapper">
-            <figure>
-              <img src="https://s3.amazonaws.com/kings-2-dev/prod_show_cart.png" />
+        </nav>
+        <section className="prod-show-middle-section">
+          <section>
+            <figure className="prod-show-main-pic-wrapper">
+              <img className="prod-show-main-pic" src={photos} />
             </figure>
-            <aside className="prod-show-like">
-              <span className="prod-show-like-child">
-                Other people want this.
-              </span>
-              15 people have this in their carts right now.
+            <div className="prod-show-main-description">
+              <h2 className="prod-show-main-description-header">Description</h2>
+              <div className="prod-show-main-description-text">
+                {description}
+              </div>
+            </div>
+          </section>
+          <div className="prod-show-form-wrapper">
+            <form className="prod-show-form">
+              <div className="prod-show-description">{description}</div>
+              <div className="prod-show-price-quest">
+                <p className="prod-show-price">{price}</p>
+                <p className="prod-ask-question">Ask a question</p>
+              </div>
+              <div className="prod-show-select-size">
+                <h3 className="prod-show-size">Size</h3>
+                <select className="custom-select" type="size">
+                  <option value="Select an option">Select an option</option>
+                  <option value="S">S</option>
+                  <option value="M">M</option>
+                  <option value="L">L</option>
+                </select>
+              </div>
+              <div className="prod-show-select-quantity">
+                <h3 className="prod-show-quantity">Quanity</h3>
+                <select className="custom-select-2" type="quantity">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              <input
+                className="prod-show-form-submit"
+                type="submit"
+                value="Add to Cart"
+              />
+            </form>
+            <div className="prod-show-like-wrapper">
+              <figure>
+                <img src="https://s3.amazonaws.com/kings-2-dev/prod_show_cart.png" />
+              </figure>
+              <aside className="prod-show-like">
+                <span className="prod-show-like-child">
+                  Other people want this.
+                </span>
+                15 people have this in their carts right now.
+              </aside>
+            </div>
+            <aside className="prod-show-overview-wrapper">
+              <div className="prod-show-overview">Overview</div>
+              <ul className="prod-show-overview-ul">
+                <li>Handmade item</li>
+                <li>Can be personalized: Yes</li>
+                <li>Made to order</li>
+              </ul>
+            </aside>
+            <aside className="prod-show-overview-wrapper">
+              <div className="prod-show-overview">Shipping & returns</div>
+              <ul className="prod-show-overview-ul">
+                <li>Ready to ship in 2–4 weeks</li>
+                <li>From United States</li>
+              </ul>
             </aside>
           </div>
-          <aside className="prod-show-overview-wrapper">
-            <div className="prod-show-overview">Overview</div>
-            <ul className="prod-show-overview-ul">
-              <li>Handmade item</li>
-              <li>Can be personalized: Yes</li>
-              <li>Made to order</li>
-            </ul>
-          </aside>
-          <aside className="prod-show-overview-wrapper">
-            <div className="prod-show-overview">Shipping & returns</div>
-            <ul className="prod-show-overview-ul">
-              <li>Ready to ship in 2–4 weeks</li>
-              <li>From United States</li>
-            </ul>
-          </aside>
-        </div>
-      </section>
+        </section>
+      </div>
+    );
   }
 }
 
