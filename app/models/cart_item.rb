441da4799f5eac95481d211004763b116c251266
Jonathan Ahn <jonathanahn95@gmail.com>
2018-12-19 14:42:58 -0500

    Begin shopping cart feature

---
+++ app/models/cart_item.rb
@@ -0,0 +1,6 @@
+class CartItem < ApplicationRecord
+  validates :product_id, :user_id, :quantity, presence: true
+
+  belongs_to :user
+  belongs_to :product
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   validates :title, :description, :price,  presence: true
 
   has_many_attached :photos
+  has_many :cart_items
+
   belongs_to :category
   belongs_to :user
 end
